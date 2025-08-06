3df3cf84fc5257c1bdc6790f583379ab283de9e6
Luna <iqromaulanahadi@gmail.com>
2025-08-06 19:20:14 +0700

    0.0.6

---
+++ home.nix
@@ -0,0 +1,50 @@
+{ config, pkgs, lib, ... }:
+
+{
+  home.packages = with pkgs; [
+    # Non GUI Programs 
+    htop
+    fastfetch
+    xclip
+    # GUI Programs
+    thunderbird
+    brave
+    telegram-desktop
+    localsend
+    appflowy
+    logseq
+    keepassxc
+    kitty
+    vscodium
+  ];
+  programs.git = {
+    enable = true;
+    userName = "John Doe";
+    userEmail = "johndoe@example.com";
+  };
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+  };
+  programs.nvf = {
+    enable = true;
+    settings.vim = {
+      theme = {
+        enable = true;
+        name = "gruvbox";
+        style = "dark";
+      };	
+    statusline.lualine.enable = true;
+    telescope.enable = true; 
+    autocomplete.nvim-cmp.enable = true;
+    lsp.enable = true;
+
+      languages = {
+        enableTreesitter = true;
+        nix.enable = true;
+        ts.enable = true;
+      };
+    };
+  };  
+  home.stateVersion = "25.05";
+}
